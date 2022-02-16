3027711e4f5dd4a42bf80425eae3eeb761f7805f
Julius Liaudanskis <julius.l@emotion.lt>
2022-02-16 23:48:09 +0200

    404 page

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,12 +1,22 @@
 import React, {useState, Fragment} from "react";
+import {Link} from "react-router-dom";
+import notFoundImage from "../images/404_nomatch.jpg";
+import NoMatchSass from "../styles/components/NoMatch.scss"
 
 function NoMatch(props) {
     return (
-        <div className="container">
+        <div className="md:w-1/2 mx-auto text-center">
             <div className="error-box">
-                <h2 className="text-2xl text-red-600">
-                    Such page was not found
-                </h2>
+                <img src={notFoundImage}
+                     alt="error 404 - page not found"
+                     title="error 404 - page not found"
+                     className="mx-auto"
+                />
+                <div className="contribute ">
+                    <Link to="http://www.freepik.com" className="text-gray-500 text-xs hover:text-purple-300">
+                        Designed by pikisuperstar / Freepik
+                    </Link>
+                </div>
             </div>
         </div>
     );
